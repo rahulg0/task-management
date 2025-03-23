a0220280c667ddd91d48377cf0f45ff9591a2bca
Rahul <imrahulg01@gmail.com>
2025-03-23 17:17:19 +0530

    Update database settings to use PostgreSQL configuration

--- backend/settings.py
+++ backend/settings.py
@@ -89,22 +89,22 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'PASSWORD': 'local@123',
-#         'HOST': 'localhost',  # Use 'localhost' if running locally
-#         'PORT': '5432',  # Default PostgreSQL port
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'local@123',
+        'HOST': 'localhost',  # Use 'localhost' if running locally
+        'PORT': '5432',  # Default PostgreSQL port
+    }
+}
 
 
 # Password validation
